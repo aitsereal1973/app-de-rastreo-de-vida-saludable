2fd23d3276abe5fb0017ce282a7d1a0d154fbcbc
aitsereal1973 <odidoquinom@hotmail.com>
2025-09-02 11:25:50 -0500

    feat: initial commit for src/components/StatisticalReport.js

---
+++ src/components/StatisticalReport.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const StatisticalReport = () => {
+  return (
+    <View>
+      <Text>Estadísticas y Reportes</Text>
+    </View>
+  );
+};
+
+export default StatisticalReport;
