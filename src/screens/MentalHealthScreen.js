a50f9a5d56f233ee13e576ea0b41e8375ac67819
aitsereal1973 <odidoquinom@hotmail.com>
2025-09-02 11:25:48 -0500

    feat: initial commit for src/screens/MentalHealthScreen.js

---
+++ src/screens/MentalHealthScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const MentalHealthScreen = () => {
+  return (
+    <View>
+      <Text>Bienestar Mental</Text>
+    </View>
+  );
+};
+
+export default MentalHealthScreen;
