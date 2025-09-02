7ed1634df1fde657de8ea521d528f4870a3b8c3d
aitsereal1973 <odidoquinom@hotmail.com>
2025-09-02 11:25:47 -0500

    feat: initial commit for src/screens/ExerciseLogScreen.js

---
+++ src/screens/ExerciseLogScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ExerciseLogScreen = () => {
+  return (
+    <View>
+      <Text>Seguimiento del Ejercicio</Text>
+    </View>
+  );
+};
+
+export default ExerciseLogScreen;
