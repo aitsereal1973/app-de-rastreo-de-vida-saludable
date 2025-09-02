b32fec56281b76b362c867386857a95739b2e742
aitsereal1973 <odidoquinom@hotmail.com>
2025-09-02 11:25:45 -0500

    feat: initial commit for src/screens/FoodLogScreen.js

---
+++ src/screens/FoodLogScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const FoodLogScreen = () => {
+  return (
+    <View>
+      <Text>Registro de Alimentos</Text>
+    </View>
+  );
+};
+
+export default FoodLogScreen;
