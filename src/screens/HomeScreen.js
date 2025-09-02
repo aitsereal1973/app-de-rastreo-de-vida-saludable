e6a0da1b7c334fc749331ca6acb25aab2f09775c
aitsereal1973 <odidoquinom@hotmail.com>
2025-09-02 11:25:44 -0500

    feat: initial commit for src/screens/HomeScreen.js

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Bienvenido a la App de Rastreo de Vida Saludable</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
